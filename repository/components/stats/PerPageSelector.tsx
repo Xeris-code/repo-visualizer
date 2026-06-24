597b669ff212a851adbdd3c970638b11ace90ae8
Peter Čišovský <xeris>
2026-06-24 23:17:15 +0200

    fixes 2

--- repository/components/stats/PerPageSelector.tsx
+++ repository/components/stats/PerPageSelector.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState, useEffect } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronUp } from "lucide-react";
 
 type PerPageSelectorProps = {
     perPage: number;
